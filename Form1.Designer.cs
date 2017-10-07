28d5620632fd6e6bbafe93a1affbba93afc89fe2
RunTimeError2 <songjiahao980530@126.com>
2017-10-07 23:51:24 +0800

    old version, unchanged

---
+++ Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Rijndael
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnFileEncrypt = new System.Windows.Forms.Button();
+            this.btnFileDecrypt = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(502, 327);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 351);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Password：";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(83, 348);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(223, 21);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(312, 348);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(98, 21);
+            this.btnEncrypt.TabIndex = 3;
+            this.btnEncrypt.Text = "Text Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(416, 348);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(98, 21);
+            this.btnDecrypt.TabIndex = 4;
+            this.btnDecrypt.Text = "Text Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 380);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "File Path：";
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(83, 377);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(361, 21);
+            this.txtFilePath.TabIndex = 6;
+            // 
+            // btnFileEncrypt
+            // 
+            this.btnFileEncrypt.Location = new System.Drawing.Point(312, 404);
+            this.btnFileEncrypt.Name = "btnFileEncrypt";
+            this.btnFileEncrypt.Size = new System.Drawing.Size(98, 21);
+            this.btnFileEncrypt.TabIndex = 7;
+            this.btnFileEncrypt.Text = "File Encrypt";
+            this.btnFileEncrypt.UseVisualStyleBackColor = true;
+            this.btnFileEncrypt.Click += new System.EventHandler(this.btnFileEncrypt_Click);
+            // 
+            // btnFileDecrypt
+            // 
+            this.btnFileDecrypt.Location = new System.Drawing.Point(416, 404);
+            this.btnFileDecrypt.Name = "btnFileDecrypt";
+            this.btnFileDecrypt.Size = new System.Drawing.Size(98, 21);
+            this.btnFileDecrypt.TabIndex = 8;
+            this.btnFileDecrypt.Text = "File Decrypt";
+            this.btnFileDecrypt.UseVisualStyleBackColor = true;
+            this.btnFileDecrypt.Click += new System.EventHandler(this.btnFileDecrypt_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(450, 377);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(64, 21);
+            this.btnOpenFile.TabIndex = 9;
+            this.btnOpenFile.Text = "Browse";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 434);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.btnFileDecrypt);
+            this.Controls.Add(this.btnFileEncrypt);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Rijndael - Key=256Bit";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnFileEncrypt;
+        private System.Windows.Forms.Button btnFileDecrypt;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Rijndael
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("Rijndael.dll", EntryPoint = "ArrayEncrypt", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void ArrayEncrypt(ref byte password, int pw_length, ref byte arr_plaintext, ref byte arr_ciphertext, int arr_length);
+        [DllImport("Rijndael.dll", EntryPoint = "ArrayDecrypt", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void ArrayDecrypt(ref byte password, int pw_length, ref byte arr_ciphertext, ref byte arr_plaintext, int arr_length);
+
+        public byte[] StringEncryptToArray(string password,string plaintext)
+        {
+            byte[] arr_pw = Encoding.UTF8.GetBytes(password);
+            byte[] arr_tmp = Encoding.UTF8.GetBytes(plaintext);
+            byte[] arr_plaintext;
+            if (arr_tmp.Length % 16 == 0)
+                arr_plaintext = arr_tmp;
+            else
+            {
+                arr_plaintext = new byte[(arr_tmp.Length / 16 + 1) * 16];
+                for (int i = 0; i < (arr_tmp.Length / 16 + 1) * 16; i++)
+                    arr_plaintext[i] = (byte)((i < arr_tmp.Length) ? (arr_tmp[i]) : 0);
+            }
+            byte[] ans = new byte[arr_plaintext.Length];
+            /*for (int i = 0; i < arr_length; i++)
+                if (i < plaintext.Length)
+                    arr_plaintext[i] = (byte)plaintext[i];
+                else
+                    arr_plaintext[i] = 0;*/
+            ArrayEncrypt(ref arr_pw[0], password.Length, ref arr_plaintext[0], ref ans[0], arr_plaintext.Length);
+            return ans;
+        }
+
+        public string ArrayDecryptToString(string password,byte[] ciphertext)
+        {
+            string ans = "";
+            byte[] arr_pw = Encoding.UTF8.GetBytes(password);
+            byte[] arr_plaintext = new byte[ciphertext.Length];
+            ArrayDecrypt(ref arr_pw[0], password.Length, ref ciphertext[0], ref arr_plaintext[0], ciphertext.Length);
+            ans = Encoding.UTF8.GetString(arr_plaintext);
+            return ans;
+        }
+
+        public string ArrayToHexString(byte[] arr)
+        {
+            string ans = "";
+            string num = "0123456789ABCDEF";
+            for(int i=0;i<arr.Length;i++)
+            {
+                ans = ans + (char)num[arr[i] / 16] + (char)num[arr[i] % 16];
+                if ((i + 1) % 16 == 0)
+                    ans = ans + " ";
+            }
+            return ans;
+        }
+
+        public byte HexCharToByte(char ch)
+        {
+            if ('0' <= ch && ch <= '9')
+                return (byte)(ch - 48);
+            else
+                return (byte)(ch - 55);
+        }
+
+        public byte[] HexStringToArray(string str)
+        {
+            byte[] ans = new byte[(str.Length % 33 == 0) ? (str.Length / 33 * 16) : ((str.Length / 33 + 1) * 16)];
+            for(int len=0,i=0;len<str.Length;i++)
+            {
+                ans[i] = (byte)(16 * HexCharToByte(str[len]) + HexCharToByte(str[len + 1]));
+                len += 2;
+                if (len < str.Length && str[len] == ' ')
+                    len++;
+            }
+            return ans;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "所有文件(*.*)|*.*";
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            string plaintext, password;
+            plaintext = textBox1.Text;
+            password = txtPassword.Text;
+            if (plaintext == "") 
+            {
+                MessageBox.Show("Text should not be empty");
+                return;
+            }
+            if (password == "") 
+            {
+                MessageBox.Show("Password should not be empty");
+                return;
+            }
+            byte[] ans = StringEncryptToArray(password, plaintext);
+            textBox1.Text = ArrayToHexString(ans);
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Text should not be empty");
+                return;
+            }
+            int count = 0;
+            for (int i = 0; i < textBox1.Text.Length; i++)
+                if (textBox1.Text[i] != ' ' && (textBox1.Text[i] > 'F' || textBox1.Text[i] < '0' || ('9' < textBox1.Text[i] && textBox1.Text[i] < 'A')))
+                {
+                    MessageBox.Show("Invalid Hex ciphertext in the textbox");
+                    return;
+                }
+                else
+                    if (textBox1.Text[i] != ' ')
+                        count++;
+            if (count % 16 != 0 || count == 0) 
+            {
+                MessageBox.Show("Invalid Hex ciphertext in the textbox");
+                return;
+            }
+            if (txtPassword.Text == "") 
+            {
+                MessageBox.Show("Password should not be empty");
+                return;
+            }
+            byte[] ciphertext = HexStringToArray(textBox1.Text);
+            string ans = ArrayDecryptToString(txtPassword.Text, ciphertext);
+            textBox1.Text = ans;
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            txtFilePath.Text = openFileDialog1.FileName;
+        }
+
+        private void btnFileEncrypt_Click(object sender, EventArgs e)
+        {
+            byte[] arr_pw;
+            if (txtPassword.Text != "")
+                arr_pw = Encoding.UTF8.GetBytes(txtPassword.Text);
+            else
+            {
+                MessageBox.Show("Password should not be empty");
+                return;
+            }
+            //==================
+            FileStream fs_r = new FileStream(txtFilePath.Text, FileMode.Open);
+            FileStream fs_w = new FileStream(txtFilePath.Text + ".renc", FileMode.Create);
+            BinaryReader br = new BinaryReader(fs_r);
+            BinaryWriter bw = new BinaryWriter(fs_w);
+            bw.Seek(0, SeekOrigin.Begin);
+            byte rest = (byte)(fs_r.Length % 16);
+            bw.Write(rest);
+            for (long i = 0; i < fs_r.Length / 16; i++)
+            {
+                byte[] plaintext = br.ReadBytes(16);
+                byte[] ciphertext = new byte[16];
+                ArrayEncrypt(ref arr_pw[0], arr_pw.Length, ref plaintext[0], ref ciphertext[0], 16);
+                bw.Write(ciphertext);
+            }
+            if (rest != 0)
+            {
+                byte[] plaintext, ciphertext;
+                plaintext = ciphertext = new byte[16] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+                for (int i = 0; i < rest; i++)
+                    plaintext[i] = br.ReadByte();
+                ArrayEncrypt(ref arr_pw[0], arr_pw.Length, ref plaintext[0], ref ciphertext[0], 16);
+                bw.Write(ciphertext);
+            }
+            MessageBox.Show("File successfully encrypted.");
+            br.Close();
+            bw.Close();
+            fs_r.Close();
+            fs_w.Close();
+
+        }
+
+        private void btnFileDecrypt_Click(object sender, EventArgs e)
+        {
+            byte[] arr_pw;
+            if (txtPassword.Text != "")
+                arr_pw = Encoding.UTF8.GetBytes(txtPassword.Text);
+            else
+            {
+                MessageBox.Show("Password should not be empty");
+                return;
+            }
+            //===============
+            FileStream fs_r = new FileStream(txtFilePath.Text, FileMode.Open);
+            FileStream fs_w;
+            if (txtFilePath.Text.EndsWith(".renc"))
+                fs_w = new FileStream(txtFilePath.Text.TrimEnd(".renc".ToCharArray()), FileMode.Create);
+            else
+                fs_w = new FileStream(txtFilePath.Text + ".rdec", FileMode.Create);
+            if ((fs_r.Length - 1) % 16 != 0)
+            {
+                MessageBox.Show("Not valid Encrypted file.");
+                fs_r.Close();
+                fs_w.Close();
+                return;
+            }
+            BinaryReader br = new BinaryReader(fs_r);
+            BinaryWriter bw = new BinaryWriter(fs_w);
+            bw.Seek(0, SeekOrigin.Begin);
+            byte rest = br.ReadByte();
+            
+            for (long i = 0; i < fs_r.Length / 16; i++) 
+            {
+                byte[] ciphertext = br.ReadBytes(16);
+                byte[] plaintext = new byte[16];
+                ArrayDecrypt(ref arr_pw[0], arr_pw.Length, ref ciphertext[0], ref plaintext[0], 16);
+                if(i<fs_r.Length /16-1)
+                    bw.Write(plaintext);
+                else
+                    for (byte j = 0; j < rest; j++)
+                        bw.Write(plaintext[j]);
+            }
+            MessageBox.Show("File successfully decrypted.");
+            br.Close();
+            bw.Close();
+            fs_r.Close();
+            fs_w.Close();
+        }
+        
+    }
+}
